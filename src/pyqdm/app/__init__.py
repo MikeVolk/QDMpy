09cd6df06516648af798d210290005dbfd2b86e4
Mike Volk <m.w.r.volk@uu.nl>
2022-08-18 13:20:40 +0200

    pep8 changes

--- src/pyqdm/app/__init__.py
+++ src/pyqdm/app/__init__.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 from logging.config import dictConfig, fileConfig
 import pyqdm
+import os
 fileConfig(os.path.join(pyqdm.projectdir, "logging.conf"))
 LOG = getLogger("pyqdm")
