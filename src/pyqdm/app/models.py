28ab6dafc97d40933b596b039cde17625f4a2a11
Mike Volk <m.w.r.volk@uu.nl>
2022-08-19 10:46:22 +0200

    add PandasModel in models

---
+++ src/pyqdm/app/models.py
@@ -0,0 +1,23 @@
+from PySide6.QtCore import QAbstractTableModel, Qt
+
+
+class PandasModel(QAbstractTableModel):
+    def __init__(self, data):
+        QAbstractTableModel.__init__(self)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parnet=None):
+        return self._data.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid() and role == Qt.DisplayRole:
+            return str(self._data.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self._data.columns[col]
+        return None
