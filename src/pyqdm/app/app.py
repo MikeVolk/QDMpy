29b81ef4c43cbf54003729fca75c80372565b9ff
Mike Volk <m.w.r.volk@uu.nl>
2022-08-20 11:32:18 +0200

    moved pandasModel

--- src/pyqdm/app/app.py
+++ src/pyqdm/app/app.py
@@ -24,9 +24,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from pyqdm.app.models import PandasModel
+
 matplotlib.use("Agg")
 
-from PySide6.QtCore import QAbstractTableModel, QSize, Qt
+from PySide6.QtCore import QSize, Qt
 from PySide6.QtGui import QAction, QIcon, QKeySequence, QScreen
 from PySide6.QtWidgets import (
     QAbstractItemView,
@@ -841,28 +843,6 @@ class PandasWidget(QGroupBox):
             self.caller.set_current_idx(idx=int(index.data()))
             self.caller.update_marker()
             self.caller.update_pixel()
-
-
-class PandasModel(QAbstractTableModel):
-    def __init__(self, data):
-        QAbstractTableModel.__init__(self)
-        self._data = data
-
-    def rowCount(self, parent=None):
-        return self._data.shape[0]
-
-    def columnCount(self, parnet=None):
-        return self._data.shape[1]
-
-    def data(self, index, role=Qt.DisplayRole):
-        if index.isValid() and role == Qt.DisplayRole:
-            return str(self._data.iloc[index.row(), index.column()])
-        return None
-
-    def headerData(self, col, orientation, role):
-        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return self._data.columns[col]
-        return None
 
 
 def main(**kwargs):
