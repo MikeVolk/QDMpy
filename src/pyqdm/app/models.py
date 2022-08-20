80882fe2622507346c06d089721bd27c08cbdc5f
Michael Volk <michaelvolk1979@gmail.com>
2022-08-21 00:33:52 +0200

    tested and finished

--- src/pyqdm/app/models.py
+++ src/pyqdm/app/models.py
@@ -11,6 +11,7 @@ class Pix:
     Pixel Singleton class. This class is used to store the current pixel index and reference.
     Also includes methods to convert between index and xy coordinates as well as between data and image.
     """
+
     class __Pix:
         def __init__(self):
             self._idx = None  # current index in img coordinates
@@ -18,11 +19,24 @@ class Pix:
             self.img_shape = None
 
         def __str__(self):
-            return f'current index is: {self._idx} with scan dimensions: ' \
-                   f'data {self.data_shape}; ' \
-                   f'img: {self.img_shape}'
+            return f'img index: {self._idx} ({self.y}, {self.x}) ' \
+                   f'data index: {self.data_idx} ({self.data_y}, {self.data_x}) ' \
+                # f'with scan dimensions: ' \
+            # f'data {self.data_shape}; ' \
+            # f'img: {self.img_shape}'
 
-        def set_idx(self, x=None, y=None, idx=None, ref='data'):
+        def set_idx(self, x=None, y=None, idx=None, ref='img'):
+            """
+            Set the current index.
+            :param x: int, optional
+                x - coordinate in img (or data) coordinates
+            :param y: int, optional
+                y - coordinate in img (or data) coordinates
+            :param idx: int, optional
+                index in img (or data) coordinates
+            :param ref: str, optional
+                reference: 'img' or 'data'
+            """
             if ref == 'data':
                 scan_dimensions = self.data_shape
             elif ref == 'img':
@@ -34,21 +48,22 @@ class Pix:
                 self._idx = rc2idx([y, x], shape=scan_dimensions)
             elif idx is not None:
                 if ref == 'data':
-                    self._idx = idx
+                    r, c = idx2rc(idx, shape=self.data_shape)
+                    self._idx = rc2idx([r * self.bin_factor, c * self.bin_factor], shape=self.img_shape)
                 elif ref == 'img':
-                    self._idx = idx * self.bin_factor
+                    self._idx = idx
             else:
                 raise ValueError('x and y or idx must be specified')
 
         @property
         def bin_factor(self):
-            return  self.img_shape[0] / self.data_shape[0]
+            return self.img_shape[0] / self.data_shape[0]
 
         @property
         def data_idx(self):
             r, c = idx2rc(self._idx, self.img_shape)
-            return rc2idx([(r + self.bin_factor / 2) / self.bin_factor,
-                           (c + self.bin_factor / 2) / self.bin_factor],
+            return rc2idx([r / self.bin_factor,
+                           c / self.bin_factor],
                           self.data_shape)
 
         @property
@@ -96,9 +111,6 @@ class Pix:
 
     def __setattr__(self, name):
         return setattr(self.instance, name)
-
-    def set_idx(self, x=None, y=None, idx=None, ref='data'):
-        self.instance.set_idx(x=x, y=y, idx=idx, ref=ref)
 
 
 class PandasModel(QAbstractTableModel):
---
+++ tests/test_models.py
@@ -0,0 +1,67 @@
+import numpy.testing as npt
+import numpy as np
+from skimage.transform import downscale_local_mean
+import itertools
+import unittest
+from pyqdm.app.models import Pix
+
+
+class Pix_test(unittest.TestCase):
+    def setUp(self):
+        self.img_array = np.random.normal(0, 100, (6, 4))
+        self.data_array = downscale_local_mean(self.img_array, (2, 2))
+
+    def test_several_instances(self):
+        p0 = Pix()
+        p0.data_shape = self.data_array.shape
+        p0.img_shape = self.img_array.shape
+        p0.set_idx(x=5, y=5)
+
+        p1 = Pix()
+        npt.assert_array_equal(p0.data_shape, p1.data_shape)
+        npt.assert_array_equal(p0.img_shape, p1.img_shape)
+        npt.assert_array_equal(p0.idx, p1.idx)
+
+        p1.set_idx(x=5, y=5)
+        npt.assert_equal(p0.idx, p1.idx)
+
+    def test_set_idx(self):
+
+        p0 = Pix()
+        p0.data_shape = self.data_array.shape
+        p0.img_shape = self.img_array.shape
+
+        for i in range(self.data_array.size):
+            data_y = np.unravel_index(i, self.data_array.shape)[0]
+            data_x = np.unravel_index(i, self.data_array.shape)[1]
+            p0.set_idx(idx=i, ref='data')
+            self.assertEqual(i, p0.data_idx)
+            self.assertEqual(data_x, p0.data_x)
+            self.assertEqual(data_y, p0.data_y)
+
+        for i in range(self.img_array.size):
+            img_y = np.unravel_index(i, self.img_array.shape)[0]
+            img_x = np.unravel_index(i, self.img_array.shape)[1]
+            p0.set_idx(idx=i, ref='img')
+            self.assertEqual(i, p0.idx)
+            self.assertEqual(img_x, p0.x)
+            self.assertEqual(img_y, p0.y)
+
+        for y, x in itertools.product(np.arange(self.data_array.shape[0],
+                                                self.data_array.shape[1])):
+            p0.set_idx(x=x, y=y, ref='data')
+            self.assertEqual(x, p0.data_x)
+            self.assertEqual(y, p0.data_y)
+            self.assertEqual(np.ravel_multi_index((y, x), self.data_array.shape), p0.data_idx)
+
+        for y, x in itertools.product(np.arange(self.img_array.shape[0],
+                                                self.img_array.shape[1])):
+            p0.set_idx(x=x, y=y, ref='img')
+            self.assertEqual(x, p0.x)
+            self.assertEqual(y, p0.y)
+            self.assertEqual(np.ravel_multi_index((y, x), self.img_array.shape), p0.idx)
+
+        with self.assertRaises(ValueError):
+            p0.set_idx(x=data_x, y=data_y, ref='notimplemented')
+        with self.assertRaises(ValueError):
+            p0.set_idx()
