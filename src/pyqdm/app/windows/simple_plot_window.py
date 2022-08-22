6907457808cc1414a0a4929aac6df9f5fdfc46f9
Mike Volk <m.w.r.volk@uu.nl>
2022-08-22 23:17:47 +0200

    simple plot with new canvas

--- src/pyqdm/app/windows/simple_plot_window.py
+++ src/pyqdm/app/windows/simple_plot_window.py
@@ -1,54 +1,24 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import colors
-from PySide6.QtWidgets import QWidget
-
 from pyqdm.app.canvas import SimpleCanvas
 from pyqdm.app.windows.pyqdm_plot_window import PyQdmWindow
 
 
 class SimplePlotWindow(PyQdmWindow):
-    def __init__(self, title, cmap, cbar, caller, *args, **kwargs):
-        canvas = SimpleCanvas(self, width=12, height=12, dpi=100, cax=cbar)
-        super().__init__(caller, canvas, *args, **kwargs)
+    def __init__(self, dtype, *args, **kwargs):
+        canvas = SimpleCanvas(dtype=dtype)
 
-        self.cmap = cmap
-        self.title = title
-        self.cbar = None
-        self.ax = self.canvas.ax
-        self.cax = self.canvas.cbar
+        super().__init__(canvas=canvas, *args, **kwargs)
+        self.set_main_window()
+        if dtype == "laser":
+            self.canvas.add_laser(self.qdm.laser, self.qdm.data_shape)
+            self.canvas.fig.subplots_adjust(top=0.97, bottom=0.124, left=0.049, right=0.951, hspace=0.2, wspace=0.2)
+            self.setWindowTitle("Laser scan")
+        elif dtype == "light":
+            self.canvas.add_light(self.qdm.light, self.qdm.data_shape)
+            self.setWindowTitle("Reflected light")
+        else:
+            raise ValueError(f"dtype {dtype} not recognized")
 
-        self.setWindowTitle(f"{title}")
-
-        self._includes_fits = False
-        self._pixel_ax = []
-        self._init_lines()
-
-        mainWidget = QWidget()
-        mainWidget.setLayout(self.mainVerticalLayout)
-        self.setCentralWidget(mainWidget)
-        self.resize(1080, 720)
+        self.canvas.add_scalebars(self.qdm.pixel_size)
         self.update_marker()
-
-    def update_img_plots(self):
-        img = self.ax.images[0]
-        d = img.get_array()
-        vmin, vmax = np.min(d), np.max(d)
-
-        if self.fix_clim_check_box.isChecked():
-            vmin, vmax = np.percentile(d, [100 - self.clims_selector.value(), self.clims_selector.value()])
-
-        img.set(norm=colors.Normalize(vmin=vmin, vmax=vmax))
-
-        if self.cbar is not None:
-            self.canvas.cbar.clear()
-            self.canvas.cbar.set_axes_locator(self.canvas.original_cax_locator)
-
-            self.cbar = plt.colorbar(
-                img,
-                cax=self.canvas.cax,
-                extend="both" if self.fix_clim_check_box.isChecked() else "neither",
-                label=r"B$_{111}$ [$\mu$T]",
-            )
-
+        self.canvas.set_img()
         self.canvas.draw()
