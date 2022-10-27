ddf9ba2d3d78079e8efc8c339ed06783ee472a4a
Mike Volk <m.w.r.volk@uu.nl>
2022-10-27 11:49:06 +0200

    removed pyqdm folder

--- src/pyqdm/app/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from logging import getLogger
-from logging.config import fileConfig
-
-import pyqdm
-
-fileConfig(os.path.join(pyqdm.projectdir, "logging.conf"))
-LOG = getLogger("pyqdm")
--- src/pyqdm/exceptions.py
+++
@@ -1,6 +0,0 @@
-class CantImportError(Exception):
-    """Base class for other exceptions"""
-
-
-class WrongFileNumber(Exception):
-    """Base class for other exceptions"""
